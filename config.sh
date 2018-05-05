c980855bde888040ef0ff16bfdf4069cb069a7f8
madScript01 <madscript01official@gmail.com>
2018-05-05 23:24:03 +0100

    Started Developing the Module

--- config.sh
+++ config.sh
@@ -40,9 +40,18 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
-  ui_print "*******************************"
+  ui_print "##################################################"
+  ui_print "##                                              ##"
+  ui_print "##  88      a8P         db        88        88  ##"
+  ui_print "##  88    .88'         d88b       88        88  ##"
+  ui_print "##  88   88'          d8''8b      88        88  ##"
+  ui_print "##  88 d88           d8'  '8b     88        88  ##"
+  ui_print "##  8888'88.        d8YaaaaY8b    88        88  ##"
+  ui_print "##  88P   Y8b      d8''''''''8b   88        88  ##"
+  ui_print "##  88     '88.   d8'        '8b  88        88  ##"
+  ui_print "##  88       Y8b d8'          '8b 888888888 88  ##"
+  ui_print "##                                              ##"
+  ui_print "################### NetHunter ####################"
 }
 
 ##########################################################################################
